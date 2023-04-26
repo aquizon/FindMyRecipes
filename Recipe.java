7f8e6a382447b02b68c1c8c7008a47d7453503ad
aquizon <84391617+aquizon@users.noreply.github.com>
2023-04-26 12:49:29 -0400

    Add files via upload

--- Recipe.java
+++ Recipe.java
@@ -2,20 +2,42 @@ public class Recipe {
 
     private int id;
     private String name;
-
-    // other attributes: ingredients (with quantities), ingredients (without quantities), instructions
+    private String ingredients;
+    private String instructions;
+    private String link;
+    private String imgFname;
     
-    public Recipe(int id, String name) {
-      this.id = id;
-      this.name = name;
+    public Recipe(int id, String name, String ingredients, String instructions, String link, String imgFname) {
+        this.id = id;
+        this.name = name;
+        this.ingredients = ingredients;
+        this.instructions = instructions;
+        this.link = link;
+        this.imgFname = imgFname;
     }
   
     public int getId() {
-      return id;
+        return id;
     }
   
     public String getName() {
-      return name;
+        return name;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public String getImgFname() {
+        return imgFname;
     }
   
     @Override
