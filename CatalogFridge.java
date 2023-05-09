0ed8b757648eb30c5d6db6d1f0b255f96c949373
Allen Quizon <aquizon@colgate.edu>
2023-05-09 15:22:46 -0400

    Fixed Search Bar Error

--- CatalogFridge.java
+++ CatalogFridge.java
@@ -381,6 +381,7 @@ public class CatalogFridge {
     // System.out.println("entered searchBarHandler body");
     if (currWindow == "Categories") {
       switchToFilteredTableScene();
+      currWindow = "Filtered";
     }
 
     ingredientsTable.setItems(filterList(searchText));
@@ -405,7 +406,7 @@ public class CatalogFridge {
   private ObservableList<Ingredient> filterList(String searchText) {
     // System.out.println("entered filterList body");
     List<Ingredient> filteredList = new ArrayList<>();
-    if (currWindow.equals("Categories")) {
+    if (currWindow.equals("Categories") || currWindow.equals("Filtered")) {
       currIngredientList = ingredientsData;
     }
     for (Ingredient t : currIngredientList) {
